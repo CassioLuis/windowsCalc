3756cc24850296c2edf65804e333a6b5fc9a0071
Cassio Luis <cassiocaruzo@gmail.com>
2022-05-22 23:46:12 -0300

    #SOZINHO

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import '../styles/Stylecalc.module.css'
+// import './test.js';
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,43 +1,21 @@
-// import Head from 'next/head'
-// import Image from 'next/image'
-import styles from '../styles/Stylecalc.module.css'
+import styles from "../styles/Stylecalc.module.css";
 
-export default function Maincalc(){
+import { teclado } from "./test.js";
 
-    
-    return (
-	    <div className={styles.calcBody}>
-            <div className={styles.containerLeft}>
-                <div className={styles.containerTop}>0</div>
-                <div className={styles.containerBot}>
-                    <button className={`${styles.tecla} ${styles.dif}`}>%</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>CE</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>C</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>&frac12;</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>1/x</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>&chi;&sup2;</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>&radic;</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>&divide;</button>
-
-                    <button className={styles.tecla}>7</button>
-                    <button className={styles.tecla}>8</button>
-                    <button className={styles.tecla}>9</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>&times;</button>
-                    <button className={styles.tecla}>4</button>
-                    <button className={styles.tecla}>5</button>
-                    <button className={styles.tecla}>6</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>&minus;</button>
-                    <button className={styles.tecla}>1</button>
-                    <button className={styles.tecla}>2</button>
-                    <button className={styles.tecla}>3</button>
-                    <button className={`${styles.tecla} ${styles.dif}`}>&#43;</button>
-                    <button className={styles.tecla}>+/-</button>
-                    <button className={styles.tecla}>0</button>
-                    <button className={styles.tecla}>,</button>
-                    <button className={`${styles.tecla} ${styles.dif2}`}>&#61;</button>
-                </div>
-            </div>
-            <div className={styles.containerRight}></div>
+export default function Maincalc() {
+  return (
+    <div className={styles.calcBody}>
+      <div className={styles.containerLeft}>
+        <div id="visor" className={styles.containerTop}>0</div>
+        <div className={styles.containerBot}>
+          {teclado.map((atual, idx) => (
+            <button className={atual.className} onClick={atual.fn} key={idx}>
+              {atual.nome}
+            </button>
+          ))}
         </div>
-    )
+      </div>
+      <div className={styles.containerRight}></div>
+    </div>
+  );
 }
